1c3c8dc244423379e19135ee46107f1b3408baee
Rik Meijer <nostriphant@rikmeijer.nl>
2026-06-17 15:53:08 +0200

    private key is no longer needed to make a request event

--- tests/Unit/NIP47Test.php
+++ tests/Unit/NIP47Test.php
@@ -1,11 +1,8 @@
 <?php
 
 it('can create an info event', function () {
-    $key = nostriphant\NIP01\Key::generate();
-    
-    $event = nostriphant\NIP47::makeInfoEvent($key);
+    $event = nostriphant\NIP47::makeInfoEvent();
     
     expect($event->kind)->toBe(13194);
-    expect($event->pubkey)->toBe($key(nostriphant\NIP01\Key::public()));
     expect(\nostriphant\NIP01\Event::extractTagValues($event, "encryption")[0])->toContain("nip44_v2 nip04");
 });
