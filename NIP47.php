2b441b06e4838d7dd91e03e4fa2eac05f91a38ad
Rik Meijer <nostriphant@rikmeijer.nl>
2026-06-17 13:56:30 +0200

    NIP47: add key parameter to makeInfoEvent
    Change makeInfoEvent to accept a NIP01\Key so the info event can be created with the key's public key and signed by that key. The returned event is built as a Rumor with kind 13194 and an "encryption" tag set to "nip44_v2 nip04". Updated unit test to generate a key, call makeInfoEvent($key), and assert the pubkey and encryption tag are present.

--- NIP47.php
+++ NIP47.php
@@ -3,7 +3,9 @@
 namespace nostriphant;
 
 class NIP47 {
-    static function makeInfoEvent() : NIP01\Rumor {
-        return new NIP01\Rumor(time(), uniqid(), 13194, "", []);
+    static function makeInfoEvent(NIP01\Key $key) : NIP01\Event {
+        return new NIP01\Rumor(time(), $key(NIP01\Key::public()), 13194, "", [
+             ["encryption", "nip44_v2 nip04"]
+        ])($key);
     }
 }
--- tests/Unit/NIP47Test.php
+++ tests/Unit/NIP47Test.php
@@ -1,7 +1,11 @@
 <?php
 
 it('can create an info event', function () {
-    $event = nostriphant\NIP47::makeInfoEvent();
+    $key = nostriphant\NIP01\Key::generate();
+    
+    $event = nostriphant\NIP47::makeInfoEvent($key);
     
     expect($event->kind)->toBe(13194);
+    expect($event->pubkey)->toBe($key(nostriphant\NIP01\Key::public()));
+    expect(\nostriphant\NIP01\Event::extractTagValues($event, "encryption")[0])->toContain("nip44_v2 nip04");
 });
